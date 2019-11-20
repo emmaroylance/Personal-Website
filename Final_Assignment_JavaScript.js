361ea346d868172f80a92ebc1d55e4fd5fe4f106
Emma Roylance <emmers646@gmail.com>
2019-11-20 12:45:19 -0700

    JavaScript Final Project

---
+++ Final_Assignment_JavaScript.js
@@ -0,0 +1,10 @@
+
+
+// These funcitons opena nd close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
